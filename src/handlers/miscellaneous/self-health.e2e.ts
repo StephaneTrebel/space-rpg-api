6e2a9e6b0eee7cf44665a0702960c6c7f8286c31
Stéphane Trebel <stephane.trebel@advisorsla.com>
2019-05-06 20:56:38 +0200

    chore(tests) Move E2E tests next to their related scripts
    It's much easier to reason about them when they are close to what they
    actually test.

--- src/handlers/miscellaneous/self-health.e2e.ts
+++ src/handlers/miscellaneous/self-health.e2e.ts
@@ -1,13 +1,6 @@
 import tape from 'tape';
 
-import { runE2ETest, getPromisified } from './e2e-utils';
-
-tape('Server starts properly: ', (t: tape.Test) =>
-  runE2ETest(t)(test => {
-    test.pass('main SHOULD sucessfully resolve');
-    return Promise.resolve(test.end());
-  }),
-);
+import { runE2ETest, getPromisified } from '../../e2e-utils';
 
 tape('Self-health endpoint', (t: tape.Test) =>
   runE2ETest(t)(test =>
---
+++ src/index.e2e.ts
@@ -0,0 +1,10 @@
+import tape from 'tape';
+
+import { runE2ETest } from './e2e-utils';
+
+tape('Server starts properly: ', (t: tape.Test) =>
+  runE2ETest(t)(test => {
+    test.pass('main SHOULD sucessfully resolve');
+    return Promise.resolve(test.end());
+  }),
+);
