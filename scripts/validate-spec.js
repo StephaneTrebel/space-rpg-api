7f39754cb0a8fa56d90e29c8b2109d2b7aed853b
Stéphane Trebel <stephane.trebel@advisorsla.com>
2019-05-08 14:23:01 +0200

    fix(scripts) Fix OpenAPI specification validation script
    - .catch was missing
    - Added a bit more logging as well

--- scripts/validate-spec.js
+++ scripts/validate-spec.js
@@ -4,13 +4,18 @@ const yaml = require('js-yaml');
 
 var doc = yaml.safeLoad(fs.readFileSync('src/openapi.yaml', 'utf8'));
 
-return Enforcer(doc, { fullResult: true }).then(function({ error, warning }) {
-  if (!error) {
-    console.log('No errors with your document');
-    if (warning) console.warn(warning);
-    return process.exit(0);
-  } else {
-    console.error(error);
+Enforcer(doc, { fullResult: true })
+  .then(function({ error, warning }) {
+    if (!error) {
+      console.log('OpenAPI Specification is valid.');
+      if (warning) console.warn(warning);
+      return process.exit(0);
+    } else {
+      console.error(error);
+      return process.exit(1);
+    }
+  })
+  .catch(error => {
+    console.error('INVALID OPENAPI SPECIFICATION:', error);
     return process.exit(1);
-  }
-});
+  });
