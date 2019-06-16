9156a4385aebcd290d01109aa6a9daddda4e8bc0
Stéphane Trebel <stephane.trebel@advisorsla.com>
2019-06-16 17:54:49 +0200

    fix(SwaggerUI) Serve the custom index.html when requested

--- src/services/webserver/service.ts
+++ src/services/webserver/service.ts
@@ -37,6 +37,7 @@ export const spawnWebServer: SpawnWebServer = deps => api => {
       // To properly use our specification in SwaggerUI, we have to manually
       // override the index.html endpoint to use our custom one.
       .get(`${swaggerUIEndpoint}/`, (_req, res) => res.send(indexContent))
+      .get(`${swaggerUIEndpoint}/index.html`, (_req, res) => res.send(indexContent))
       // This is the main SwaggerUI endpoint, from which all static assets will
       // be loaded
       .use(`${swaggerUIEndpoint}`, deps.express.static(swaggerUIAssetPath))
