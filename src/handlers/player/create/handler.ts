9903f34d91d637b656f5b5d72dd6596d3abb9ad4
Stéphane Trebel <stephane.trebel@advisorsla.com>
2019-08-29 17:28:36 +0200

    feat(PlayerCreation) Add fluff text
    - Fixes #41

--- src/handlers/player/create/handler.ts
+++ src/handlers/player/create/handler.ts
@@ -42,6 +42,21 @@ export const addNewPlayer: AddNewPlayer = ({
 	]).then(() => ({ newPlayer, newSpaceship }));
 };
 
+type GetNewPlayerText = (deps: {
+	loggerService: LoggerService;
+}) => (params: { newPlayer: Player; newSpaceship: Spaceship }) => string;
+export const getNewPlayerText: GetNewPlayerText = ({ loggerService }) => ({
+	newPlayer,
+	newSpaceship,
+}) => {
+	loggerService.debug('Entering getNewPlayerText...');
+	return `You hear a buzz, and open your eyes. An employee helps you getting outside the Clone-O-Tron and explains you the basics for the umpteenth time: You weren't, and yet now you are. The scientific mumbo-jumbo glosses over you as your brain adapts to your new existence and suddenly a card key falls onto your lap: "Your spaceship awaits...err, ${
+		newPlayer.name
+	}, is it ? Time to take your brand new spaceship, that have been baptized ${
+		newSpaceship.name
+	} and to go wherever you want. The universe is your oyster, yaddi-yadda, you know the drill. Well you obviously don't. Have fun !"`;
+};
+
 type AddNewPlayerHandler = (deps: {
 	loggerService: LoggerService;
 	stateService: StateService;
@@ -58,6 +73,10 @@ export const addNewPlayerHandler: AddNewPlayerHandler = ({
 					links: [SELF_HEALTH_LINK],
 					player: newPlayer,
 					spaceship: newSpaceship,
+					text: getNewPlayerText({ loggerService })({
+						newPlayer,
+						newSpaceship,
+					}),
 				},
 				status: 201,
 			}),
